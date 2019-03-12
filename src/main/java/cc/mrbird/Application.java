0b5b8b17460834a75c607fe92c42bbb46109b3da
mofei <linux.root.users@gmail.com>
2019-03-12 22:36:32 +0800

    init

---
+++ src/main/java/cc/mrbird/Application.java
@@ -0,0 +1,31 @@
+package cc.mrbird;
+
+import cc.mrbird.common.config.FebsProperties;
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("cc.mrbird.*.dao")
+@EnableConfigurationProperties({FebsProperties.class})
+@EnableCaching
+@EnableAsync
+public class Application {
+
+    private static Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        log.info("《《《《《《 Telegram-bot-admin started up successfully at {} {} 》》》》》》", LocalDate.now(), LocalTime.now());
+    }
+}
